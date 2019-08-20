0374e3be416647cec360d9c85392a0b3cf19ebad
dongxun_lord <50280501+dongxun1@users.noreply.github.com>
2019-08-20 08:14:09 +0800

    Update configs.py

--- configs.py
+++ configs.py
@@ -11,8 +11,8 @@ MONGO_PORT = 27017
 
 #  Mysql params
 MYSQL_HOST = 'localhost'
-MYSQL_PASSWORD = 'bai22933'
+MYSQL_PASSWORD = '' #  自己的数据库密码
 MYSQL_PORT = 3306
 MYSQL_USERNAME = 'root'
 # 数据库名称
-MYSQL_DATABASE = 'spiders'
+MYSQL_DATABASE = 'spiders'   
